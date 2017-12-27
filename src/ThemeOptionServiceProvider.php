8c645e8d1c5365311b9ee09da95a6a787be585f9
Hieupv <hieupv@codersvn.com>
2017-12-27 14:01:34 +0700

    core

---
+++ src/ThemeOptionServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NightFury\ThemeOption;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThemeOptionServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+}
